7efc98a845271c63465927e2b1c6cf5584e0895b
Pau Calafat <pau.calafat@gmail.com>
2024-06-24 20:20:34 +0200

    Porftolio created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SidebarComponent,ContactComponent,ProjectsComponent,AboutComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const navbar = document.getElementById('navbar');
+    if (window.pageYOffset > 0) {
+      navbar!.style.display = 'flex';
+    } else {
+      navbar!.style.display = 'none';
+    }
+  }
 }
